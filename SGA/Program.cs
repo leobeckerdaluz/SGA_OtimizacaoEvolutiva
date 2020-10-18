854fca2dcd5c4a3f2b8fefef38230e361763f816
leobeckerdaluz <leobeckerdaluz@gmail.com>
2020-10-18 18:36:08 -0300

    Crossover implementado

--- SGA/Program.cs
+++ SGA/Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-// using System.Collections.Generic;
-// using System.Linq;
+using System.Collections.Generic;
+using System.Linq;
 
 
 namespace SGA
@@ -18,11 +18,31 @@ namespace SGA
             return individuo;
         }
 
+        private static List<string> Crossover(string individuo1, string individuo2){
+            // Os cromossomos possuem tamanhos iguais, então obtém uma
+            // ... posição entre 0 e o tamanho de um dos cromossomos.
+            int pos = random.Next(0, individuo1.Length);
+
+            // Cria as recombinações dos 2 novos indivíduos            
+            string crossover1 = individuo1.Substring(pos) + individuo2.Substring(0, pos);
+            string crossover2 = individuo2.Substring(pos) + individuo1.Substring(0, pos);
+            
+            // Retorna os dois novos indivíduos
+            return new List<string>{ crossover1, crossover2 };
+        }
+
         static void Main(string[] args){
             Console.WriteLine("Geração Individuo: ");
             
-            string ind = GeraIndividuo(10);
-            Console.WriteLine(ind);
+            string ind1 = GeraIndividuo(10);
+            Console.WriteLine(ind1);
+
+            string ind2 = GeraIndividuo(10);
+            Console.WriteLine(ind2);
+
+            List<string> teste = Crossover(ind1, ind2).ToList();
+            Console.WriteLine(teste[0]);
+            Console.WriteLine(teste[1]);
 
         }
     }
