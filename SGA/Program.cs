e0e44f919fde06a9898850a78f9d70c870ba30cb
leobeckerdaluz <leobeckerdaluz@gmail.com>
2020-10-19 00:20:00 -0300

    iteracao while criterio parada + primeira estrutura funcional sem selecao

--- SGA/Program.cs
+++ SGA/Program.cs
@@ -7,16 +7,9 @@ namespace SGA
 {
     public class SGA
     {
+
         private static Random random = new Random();
         
-        // public static string GeraIndividuoString(int tamanho_genotipo){
-        //     string individuo = "";
-            
-        //     for (int i=0; i<tamanho_genotipo; ++i)
-        //         individuo += ((random.Next(0, 2) == 1) ? "1" : "0");
-            
-        //     return individuo;
-        // }
 
         public static List<bool> GeraIndividuoBool(int tamanho_genotipo){
             List<bool> array = new List<bool>();
@@ -27,18 +20,6 @@ namespace SGA
             return array;
         }
 
-        // private static List<string> Crossover(string individuo1, string individuo2){
-        //     // Os cromossomos possuem tamanhos iguais, então obtém uma
-        //     // ... posição entre 0 e o tamanho de um dos cromossomos.
-        //     int pos = random.Next(0, individuo1.Length);
-
-        //     // Cria as recombinações dos 2 novos indivíduos            
-        //     string crossover1 = individuo1.Substring(pos) + individuo2.Substring(0, pos);
-        //     string crossover2 = individuo2.Substring(pos) + individuo1.Substring(0, pos);
-            
-        //     // Retorna os dois novos indivíduos
-        //     return new List<string>{ crossover1, crossover2 };
-        // }
 
         private static List<List<bool>> CrossoverBool(List<bool> individuo1, List<bool> individuo2){
             // Os cromossomos possuem tamanhos iguais, então obtém uma
@@ -63,8 +44,8 @@ namespace SGA
             return new List<List<bool>>(){crossover1, crossover2};
         }
 
-        private static List<bool> MutacaoBool(List<bool> cromossomo, double probabilidade)
-        {
+
+        private static List<bool> MutacaoBool(List<bool> cromossomo, double probabilidade){
             int len = cromossomo.Count;
 
             for(int i=0; i<len; i++){
@@ -75,6 +56,15 @@ namespace SGA
 
             return cromossomo;
         }
+
+
+        private static List<List<bool>> SelecaoPais(List<List<bool>> populacao, int tamanho_populacao){
+            List<bool> pai1 = populacao[random.Next(0, tamanho_populacao)];
+            List<bool> pai2 = populacao[random.Next(0, tamanho_populacao)];
+
+            return new List<List<bool>>(){pai1, pai2};
+        }
+
 
         private static void print_bool_array(List<bool> boolarray){
             string string_array = "";
@@ -88,52 +78,71 @@ namespace SGA
 
         static void Main(string[] args){
             double probabilidade_mutacao = 0.5;
+            double probabilidade_crossover = 0.5;
+            int tamanho_populacao = 10;
+            const int tamanho_genotipo = 10;
+            const int criterio_parada_nro_avaliacoes_funcao = 10;
 
-            Console.WriteLine("Geração Individuo: ");
+            // Geração da população
+            List<List<bool>> populacao = new List<List<bool>>();
+            for(int i=0; i<tamanho_populacao; i++){
+                populacao.Add(GeraIndividuoBool(tamanho_genotipo));
+            }
+            Console.WriteLine("População gerada!");
             
-            List<bool> ind1 = GeraIndividuoBool(10);
-            List<bool> ind2 = GeraIndividuoBool(10);
-            Console.Write("ind1: ");
-            print_bool_array(ind1);
-            Console.Write("ind2: ");
-            print_bool_array(ind2);
+            // Apresenta a população
+            for(int i=0; i<tamanho_populacao; i++)
+                print_bool_array(populacao[i]);
             
-            // string ind2 = GeraIndividuo(10);
-            // Console.WriteLine(ind2);
-
-            // List<string> teste = Crossover(ind1, ind2).ToList();
-            // Console.WriteLine(teste[0]);
-            // Console.WriteLine(teste[1]);
-
-            List<List<bool>> crossovers = CrossoverBool(ind1, ind2);
-            List<bool> crossover1 = crossovers[0];
-            List<bool> crossover2 = crossovers[1];
-            Console.Write("crossover1: ");
-            print_bool_array(crossover1);
-            Console.Write("crossover2: ");
-            print_bool_array(crossover2);
-
-            List<bool> mutado1 = MutacaoBool(ind1, probabilidade_mutacao);
-            List<bool> mutado2 = MutacaoBool(ind2, probabilidade_mutacao);
-            Console.Write("mutação do ind1: ");
-            print_bool_array(mutado1);
-            Console.Write("mutação do ind2: ");
-            print_bool_array(mutado2);
-            // string mutado1 = Mutacao(ind1, 1);
-            // string mutado2 = Mutacao(ind2, 1);
-            // Console.WriteLine(mutado1);
-            // Console.WriteLine(mutado2);
-
-
-            // List<bool> ind = new List<bool> {false, true, true, true, false, false, true};
-            // Console.WriteLine(ind);
-
-            // string array = boolarray_to_string(ind);
-            // Console.WriteLine(array);
             
-            // string result = string.Join("", arr1);
-            // Console.WriteLine(result);
+            // Loop de operações
+            int iteracoes = 0;
+            while (iteracoes < criterio_parada_nro_avaliacoes_funcao){
+                Console.WriteLine("------------------------------------------------------------------------------");
+                // Seleciona 2 da população
+                List<List<bool>> pais = SelecaoPais(populacao, tamanho_populacao);
+                List<bool> pai1 = pais[0];
+                List<bool> pai2 = pais[1];
 
+
+                // Cria os filhos
+                List<bool> filho1 = pai1;
+                List<bool> filho2 = pai2;
+
+
+                // Crossover
+                if (random.NextDouble() < probabilidade_crossover){
+                    List<List<bool>> crossovers = CrossoverBool(pai1, pai2);
+                    
+                    filho1 = crossovers[0];
+                    Console.Write("crossover1: ");
+                    print_bool_array(crossovers[0]);
+                    Console.Write("filho1: ");
+                    print_bool_array(filho1);
+
+                    filho2 = crossovers[1];
+                    Console.Write("crossover2: ");
+                    print_bool_array(crossovers[1]);
+                    Console.Write("filho2: ");
+                    print_bool_array(filho2);
+                }
+                else{
+                    Console.WriteLine("Sem Crossover!");
+                }
+
+                
+                // Mutação
+                filho1 = MutacaoBool(filho1, probabilidade_mutacao);
+                filho2 = MutacaoBool(filho2, probabilidade_mutacao);
+                Console.Write("mutação do filho1: ");
+                print_bool_array(filho1);
+                Console.Write("mutação do filho2: ");
+                print_bool_array(filho2);
+        
+
+                // Adiciona
+                iteracoes++;
+            }
         }
     }
 }
